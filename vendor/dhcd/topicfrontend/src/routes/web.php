908d7cc722ffd87f0c132fc3e03e045c586ad367
tuan <tuanlv@vnedutech.vn>
2018-07-13 13:40:00 +0700

    no message

--- vendor/dhcd/topicfrontend/src/routes/web.php
+++ vendor/dhcd/topicfrontend/src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 $adminPrefix = '';
-//$adminPrefix = config('site.admin_prefix');
+//$adminPrefix = config('site.admin_prefix');;:
 Route::group(array('prefix' => $adminPrefix), function() {
 
     Route::group(['middleware' => ['dhcd.auth']], function () {
